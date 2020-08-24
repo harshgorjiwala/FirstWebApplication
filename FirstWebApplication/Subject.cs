f8e81035e013df2df8dadbd2cb50171b01a23729
Palak <palakpatel1793@gmail.com>
2020-08-23 22:35:37 -0400

    Save file
    Palak

--- FirstWebApplication/Subject.cs
+++ FirstWebApplication/Subject.cs
@@ -3,6 +3,8 @@ namespace FirstWebApplication
 {
     public class Subject
     {
+        public int ID { get; set; }
+        public string SubjectName { get; set; }
         public Subject()
         {
         }
--- FirstWebApplication/Teacher.cs
+++ FirstWebApplication/Teacher.cs
@@ -3,6 +3,9 @@ namespace FirstWebApplication
 {
     public class Teacher
     {
+        public int ID { get; set; }
+        public string TeacherName { get; set; }
+
         public Teacher()
         {
         }

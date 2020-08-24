edf19f1e80827e4a0e244b5328ec84cea33fb25b
Palak <palakpatel1793@gmail.com>
2020-08-23 21:30:15 -0400

    Update Class.cs

--- FirstWebApplication/Class.cs
+++ FirstWebApplication/Class.cs
@@ -4,6 +4,9 @@ namespace FirstWebApplication
     // this is new class
     public class Class
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string ClassName { get; set; }
         public Class()
         {
         }

cf0bf6a11c86f1c787ee4f434cbba541f3a83e19
harshgorjiwala <harsh7529gorjiwala@gmail.com>
2020-08-25 16:28:57 -0400

    h

--- FirstWebApplication/Teacher.cs
+++ FirstWebApplication/Teacher.cs
@@ -3,10 +3,8 @@ namespace FirstWebApplication
 {
     public class Teacher
     {
-<<<<<<< HEAD
         public int id { get; set; }
         public string name { get; set; }
-=======
         public int ID { get; set; }
         public String TeacherName { get; set; }
 
@@ -20,6 +18,5 @@ namespace FirstWebApplication
         {
             return "Teacher Id:" + ID + " Teacher Name: " + TeacherName; 
         }
->>>>>>> 0a7e1fd52cda3092b8729646c30af81617b932d7
     }
 }
